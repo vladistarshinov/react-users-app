42649194ddd3c620108c7817a3aa620287f352f9
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-08-14 19:02:46 +0300

    Updates

---
+++ precache-manifest.3c9f87606b4e003e76d2dbc6185e1344.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3212f2ba32cfff815075bdcc80b49cc1",
+    "url": "/react-users-app/index.html"
+  },
+  {
+    "revision": "b518077f9f414f822b46",
+    "url": "/react-users-app/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "42a21d22b5746fbee92f",
+    "url": "/react-users-app/static/css/main.269a8ecf.chunk.css"
+  },
+  {
+    "revision": "b518077f9f414f822b46",
+    "url": "/react-users-app/static/js/2.9ebff80a.chunk.js"
+  },
+  {
+    "revision": "f20e7460d0e10ff4f29574f4f4b6f474",
+    "url": "/react-users-app/static/js/2.9ebff80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a21d22b5746fbee92f",
+    "url": "/react-users-app/static/js/main.3e3f2591.chunk.js"
+  },
+  {
+    "revision": "4adb0024e40375e71cf1",
+    "url": "/react-users-app/static/js/runtime-main.61bf08e8.js"
+  },
+  {
+    "revision": "0652e716aef3af4cd0cf494c966b7419",
+    "url": "/react-users-app/static/media/main.0652e716.jpg"
+  }
+]);
